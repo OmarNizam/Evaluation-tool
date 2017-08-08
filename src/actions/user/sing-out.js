34ba933bc73f9e67800c4dc8a8dffc3ca14097ec
OmarNizam <eng.omarnizam@gmail.com>
2017-08-08 18:09:29 +0200

    create sign in and sign out actions

---
+++ src/actions/user/sing-out.js
@@ -0,0 +1,12 @@
+// src/actions/user/sign-out.js
+
+import API from '../../api'
+
+export const USER_SIGNED_OUT = 'USER_SIGNED_OUT'
+
+const api = new API()
+
+export default () => {
+  api.signOut()
+  return { type: USER_SIGNED_OUT }
+}
