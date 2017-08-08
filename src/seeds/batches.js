dac92f4e7c682a5c88663fb70724299e629faf9f
OmarNizam <eng.omarnizam@gmail.com>
2017-08-08 12:11:06 +0200

    create seeds for batches

---
+++ src/seeds/batches.js
@@ -0,0 +1,39 @@
+// src/seeds/batches.js
+
+export default [
+  {
+    _id: '1',
+    title: 'Batch 1',
+    startDate: "August 17",
+    endDate: "September 17",
+    students: ["Mat", "Tania", "Omar", "Mojgan", "Benmar"],
+  },
+  {
+    _id: '2',
+    title: 'Batch 2',
+    startDate: "August 17",
+    endDate: "September 17",
+    students: ["Mat", "Tania", "Omar", "Mojgan", "Benmar"],
+  },
+  {
+    _id: '3',
+    title: 'Batch 3',
+    startDate: "August 17",
+    endDate: "September 17",
+    students: ["Mat", "Tania", "Omar", "Mojgan", "Benmar"],
+  },
+  {
+    _id: '4',
+    title: 'Batch 4',
+    startDate: "August 17",
+    endDate: "September 17",
+    students: ["Mat", "Tania", "Omar", "Mojgan", "Benmar"],
+  },
+  {
+    _id: '5',
+    title: 'Batch 5',
+    startDate: "August 17",
+    endDate: "September 17",
+    students: ["Mat", "Tania", "Omar", "Mojgan", "Benmar"],
+  },
+]
