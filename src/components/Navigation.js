aa993c7609f953889ff0de1eb1281d249123da9e
OmarNizam <eng.omarnizam@gmail.com>
2017-08-11 06:45:42 +0200

    fix sign out in the nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
-import signOut from '../actions/user/sing-out'
+import signOut from '../actions/user/sing-out.js'
 import AppBar from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
 
@@ -15,16 +15,22 @@ class Navigation extends PureComponent {
     this.props.push('/')
   }
 
+  signedOut() {
+    this.props.signOut()
+    this.props.push('/sign-in')
+  }
+
+
   render() {
     const { signedIn } = this.props
     const styles = { title: { cursor: 'pointer' }}
     return (
       <AppBar
-        title={<span style={styles.title}>Evaluations Tool</span>}
+        title={<span style={styles.title}>Student Evaluations</span>}
         showMenuIconButton={false}
         onTitleTouchTap={this.goHome.bind(this)}
         iconElementRight={signedIn ?
-          <FlatButton label="Sign Out" onClick={this.signOut} /> :
+          <FlatButton label="Sign Out" onClick={this.signedOut.bind(this)} /> :
           <FlatButton label="Sign In" onClick={this.signIn.bind(this)} />
         }
       />
